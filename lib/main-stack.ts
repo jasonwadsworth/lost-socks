6ce6ec9ca0211c5ac145c571007d73336e3c0113
Julia Furst Morgado <juliafmorgado@Julias-MacBook-Pro.local>
2025-11-30 13:47:45 -0800

    Add CDK infrastructure for CloudFront deployment

--- lib/main-stack.ts
+++ lib/main-stack.ts
@@ -1,10 +1,62 @@
 import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import * as path from "path";
 
 export class MainStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // AWS resources will be defined here
+        // S3 bucket for hosting the frontend
+        const websiteBucket = new s3.Bucket(this, "LostSocksWebsite", {
+            publicReadAccess: false,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
+
+        // CloudFront distribution
+        const distribution = new cloudfront.Distribution(this, "LostSocksDistribution", {
+            defaultBehavior: {
+                origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+            },
+            defaultRootObject: "index.html",
+            errorResponses: [
+                {
+                    httpStatus: 404,
+                    responseHttpStatus: 200,
+                    responsePagePath: "/index.html",
+                },
+                {
+                    httpStatus: 403,
+                    responseHttpStatus: 200,
+                    responsePagePath: "/index.html",
+                },
+            ],
+        });
+
+        // Deploy frontend build to S3
+        new s3deploy.BucketDeployment(this, "DeployWebsite", {
+            sources: [s3deploy.Source.asset(path.join(__dirname, "../frontend/dist"))],
+            destinationBucket: websiteBucket,
+            distribution,
+            distributionPaths: ["/*"],
+        });
+
+        // Output the CloudFront URL
+        new cdk.CfnOutput(this, "WebsiteURL", {
+            value: `https://${distribution.distributionDomainName}`,
+            description: "Lost Socks Website URL",
+        });
+
+        new cdk.CfnOutput(this, "DistributionId", {
+            value: distribution.distributionId,
+            description: "CloudFront Distribution ID",
+        });
     }
 }
